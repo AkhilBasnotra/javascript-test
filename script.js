4c6d217b032fa647de5bd0df358f74333d590408
Akhil Basnotra <akhilbasnotra24@gmail.com>
2023-12-16 02:12:21 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 document.addEventListener("DOMContentLoaded", loadTodo);
+
 let btn = document.querySelector("#btn");
 let addedTodos = document.querySelector(".addedTodos");
 let donetodo = document.querySelector(".donetodo");
-let url = "https://crudcrud.com/api/685cbefe4d9748f2bad14627c1de425d/todos";
+let url = "https://crudcrud.com/api/4f1a6603454e4d94a48f0cd6355e91a9/todos";
 
 btn.addEventListener("click", addTodo);
 
-function addTodo(e) {
-  let todo = document.querySelector("#todo-name").value;
+function addTodo() {
+  let todoName = document.querySelector("#todo-name").value;
   let description = document.querySelector("#description").value;
 
-  let mytodo = {
-    todo,
+  let myTodo = {
+    todoName,
     description,
     isTodoDone: false,
   };
 
   axios
-    .post(url, mytodo)
+    .post(url, myTodo)
     .then((response) => {
-      newTodo(response.data);
+      console.log(response);
+      responseTodo(response.data, addedTodos);
     })
     .catch((error) => {
       console.log(error);
@@ -30,9 +32,15 @@ function loadTodo() {
   axios
     .get(url)
     .then((response) => {
+      console.log(response);
       let items = response.data;
-      items.forEach((element) => {
-        newTodo(element);
+
+      items.forEach((todo) => {
+        if (!todo.isTodoDone) {
+          responseTodo(todo, addedTodos); // Pass the container for remaining todos
+        } else {
+          responseTodo(todo, donetodo); // Pass the container for done todos
+        }
       });
     })
     .catch((error) => {
@@ -41,74 +49,67 @@ function loadTodo() {
 }
 
 function removeFromServer(li) {
-  return new Promise((resolve, reject) => {
-    axios
-      .delete(`${url}/${li.dataset.id}`)
-      .then((response) => {
-        resolve(response.data);
-      })
-      .catch((error) => {
-        reject(error);
-      });
-  });
+  axios
+    .delete(`${url}/${li.dataset.id}`)
+    .then((response) => {
+      li.parentElement.removeChild(li);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-function removeItem(e) {
-  if (e.target.classList.contains("delbtn")) {
-    if (confirm("Are you sure")) {
-      let li = e.target.parentElement;
-      addedTodos.removeChild(li);
-      removeFromServer(li);
-    }
-  }
+function taskdone(li) {
+  axios
+    .put(`${url}/${li.dataset.id}`, {
+      todoName: li.dataset.todoName,
+      description: li.dataset.description,
+      isTodoDone: true,
+    })
+    .then((response) => {
+      donetodo.appendChild(li);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-function newTodo(mytodo) {
+function responseTodo(data, container) {
   let li = document.createElement("li");
-  li.dataset.id = mytodo._id;
-  li.appendChild(
-    document.createTextNode(`${mytodo.todo} => ${mytodo.description}`)
+  li.dataset.id = data._id;
+  li.dataset.todoName = data.todoName;
+  li.dataset.description = data.description;
+
+  let textNode = document.createTextNode(
+    `${data.todoName} => ${data.description}`
   );
+  li.appendChild(textNode);
 
-  let donebtn = document.createElement("button");
-  donebtn.classList = "done";
-  donebtn.appendChild(document.createTextNode(`✅`));
-  donebtn.addEventListener("click", taskDone);
+  let doneTodoBtn = document.createElement("button");
+  doneTodoBtn.classList = "done";
+  doneTodoBtn.appendChild(document.createTextNode("✅"));
+  doneTodoBtn.addEventListener("click", doneTodo);
 
-  let delBtn = document.createElement("button");
-  delBtn.classList = "delbtn";
-  delBtn.appendChild(document.createTextNode("❌"));
-  delBtn.addEventListener("click", removeItem);
+  let delTodo = document.createElement("button");
+  delTodo.classList = "delTodo";
+  delTodo.appendChild(document.createTextNode("❌"));
+  delTodo.addEventListener("click", removeTodo);
 
-  li.appendChild(donebtn);
-  li.appendChild(delBtn);
-  addedTodos.appendChild(li);
+  li.appendChild(doneTodoBtn);
+  li.appendChild(delTodo);
+  container.appendChild(li);
 }
 
-function taskDone(e) {
+function doneTodo(e) {
   if (e.target.classList.contains("done")) {
     let li = e.target.parentElement;
-    let todoId = li.dataset.id;
-
-    axios
-      .put(`${url}/${todoId}`, { isTodoDone: true })
-      .then((response) => {
-        moveTodoToDown(li);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    taskdone(li);
   }
 }
 
-function moveTodoToDown(li) {
-  li.removeChild(li.querySelector(".done"));
-  li.removeChild(li.querySelector(".delbtn"));
-
-  let newLi = document.createElement("li");
-  newLi.appendChild(document.createTextNode(li.textContent));
-
-  donetodo.appendChild(newLi);
-
-  addedTodos.removeChild(li);
+function removeTodo(e) {
+  if (e.target.classList.contains("delTodo")) {
+    let li = e.target.parentElement;
+    removeFromServer(li);
+  }
 }
